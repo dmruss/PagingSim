cd11cc6acd25d1c0f86d0985f5b4916930a4d5f8
David Russell <druss337@gmail.com>
2020-12-04 17:15:44 -0700

    start lru

--- PageReplace.h
+++ PageReplace.h
@@ -13,15 +13,11 @@ Queue* Fifo(Queue* pageList, int frames) {
   int numPageFaults = 0;
 
   while (pageList->size > 0) {
-
-
     int pagesExec = 10000 - pageList->size;
     int currentPage = pageList->front->data;
     bool pageInMemory = frameList->searchFor(currentPage);
 
-
-    if (pagesExec % 2000 == 0 && pagesExec != 10000) {pageFaults->Enqueue(numPageFaults);}
-
+    if (pagesExec % 2000 == 0 && pagesExec != 0) {pageFaults->Enqueue(numPageFaults);}
 
     if (pageInMemory == false) {
       if (frameList->size < frames) {
@@ -33,21 +29,56 @@ Queue* Fifo(Queue* pageList, int frames) {
         frameList->deQueue();
         frameList->Enqueue(pageList->front->data);
         pageList->deQueue();
-
         numPageFaults += 1;
       }
-
     }
     else if (pageInMemory == true) {
       pageList->deQueue();
     }
   }
-
   pageFaults->Enqueue(numPageFaults);
-
-
-
   return pageFaults;
+}
+
+Queue* LRU(Queue* pageList, int frames) {
+
+    Queue* pageFaults = new Queue();
+    Queue* frameList = new Queue();
+    int numPageFaults = 0;
+
+
+    while (pageList->size > 0) {
+      int pagesExec = 10000 - pageList->size;
+      int currentPage = pageList->front->data;
+      bool nodeBeforeFound = frameList->searchForNode(currentPage);
+
+      if (pagesExec % 2000 == 0 && pagesExec != 0) {pageFaults->Enqueue(numPageFaults);}
+
+      if (nodeBeforeFound == NULL) {
+        if (frameList->size < frames) {
+          frameList->Enqueue(pageList->front->data);
+          pageList->deQueue();
+          numPageFaults += 1;
+        }
+        else if (frameList->size == frames) {
+          frameList->deQueue();
+          frameList->Enqueue(pageList->front->data);
+          pageList->deQueue();
+          numPageFaults += 1;
+        }
+      }
+      else if (nodeBeforeFound != NULL) {
+        //MOVETOTAILINFRAMELIST
+        pageList->deQueue();
+        /*if (nodeBeforeFound->data == currentPage) {
+          frameList->recentlyUsedFront(nodeBeforeFound);
+        }else if (nodeBeforeFound->data != currentPage) {
+          frameList->recentlyUsedMiddle(nodeBeforeFound);
+        }*/
+      }
+    }
+    pageFaults->Enqueue(numPageFaults);
+    return pageFaults;
 }
 
 
